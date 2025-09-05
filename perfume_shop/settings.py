b4a5822491803c162635c69d3adaac4a44524dda
Mr - A N A S <mr.anas1003@gmail.com>
2025-09-05 22:30:52 +0500

    Added STATIC_ROOT and STATICFILES_STORAGE back.

--- perfume_shop/settings.py
+++ perfume_shop/settings.py
@@ -135,6 +135,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# WhiteNoise settings
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
