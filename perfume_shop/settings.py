9ca6135cfe2449e40c8aae6409aba11e7057c980
Mr - A N A S <mr.anas1003@gmail.com>
2025-09-05 22:35:27 +0500

    Changed STATIC_URL to '/static/' with leading slash

--- perfume_shop/settings.py
+++ perfume_shop/settings.py
@@ -133,12 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_URL = '/static/'
+STATICFILES_DIRS = ['static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-
-# WhiteNoise settings
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
