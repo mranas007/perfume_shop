eaeac32c0943a8e4b2998054e6f30128ceebbb85
Mr - A N A S <mr.anas1003@gmail.com>
2025-10-29 22:05:15 +0500

    add sh

--- start.sh
+++ start.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Running database migrations..."
-python manage.py migrate --noinput
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
 echo "Starting Gunicorn..."
 exec gunicorn perfume_shop.wsgi:application \
     --bind 0.0.0.0:8000 \
