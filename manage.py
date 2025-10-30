ed37dbe0a349acb4fb9337427fb1bce825f5b7c9
Mr - A N A S <mr.anas1003@gmail.com>
2025-10-30 11:06:34 +0500

    update proc and wsgi, asgi and manaer for prod

--- manage.py
+++ manage.py
@@ -2,11 +2,14 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
 
+# Load environment variables
+dotenv.load_dotenv()
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perfume_shop.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE', 'perfume_shop.settings.development'))
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- perfume_shop/wsgi.py
+++ perfume_shop/wsgi.py
@@ -2,6 +2,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'perfume_shop.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.getenv('DJANGO_SETTINGS_MODULE','perfume_shop.settings.development'))
 
 application = get_wsgi_application()
